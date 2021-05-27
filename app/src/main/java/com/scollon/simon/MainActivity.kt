e3b25c51f2ff64bd8657e5d0f3a3a63d826d9e8f
TalekC-137 <pawatl2002@gmail.com>
2021-05-28 00:05:22 +0200

    updated inner workings

--- app/src/main/java/com/scollon/simon/MainActivity.kt
+++ app/src/main/java/com/scollon/simon/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.scollon.simon
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
@@ -10,6 +11,7 @@ import androidx.core.view.isVisible
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.*
 import java.util.concurrent.TimeUnit
+import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
 
@@ -24,9 +26,12 @@ class MainActivity : AppCompatActivity() {
         button2.setBackgroundResource(R.drawable.kafelek);
         button3.setBackgroundResource(R.drawable.kafelek);
         button4.setBackgroundResource(R.drawable.kafelek);
+        button.isClickable = false
+        button2.isClickable = false
+        button3.isClickable = false
+        button4.isClickable = false
 
-
-        for(i in 0..10){
+        for(i in 0..100){
             seq += (1..4).random()
         }
 
@@ -35,15 +40,14 @@ class MainActivity : AppCompatActivity() {
             btn_run.visibility  = View.GONE
         }
 
+        btn_again.setOnClickListener(){
+            finish()
+            startActivity(getIntent())
+            overridePendingTransition(R.anim.abc_fade_in,R.anim.abc_fade_out);
+        }
 
         val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, seq)
         lista.adapter = adapter
-
-
-
-
-
-
 
     }
 
@@ -80,6 +84,10 @@ class MainActivity : AppCompatActivity() {
             } else {
                 // player made a mistake
                 Toast.makeText(this, "źle", Toast.LENGTH_LONG).show()
+                var beatenScore = rounds-1
+                tv_score.text = beatenScore.toString()
+               cimno.visibility = View.VISIBLE
+                end_game.visibility = View.VISIBLE
             }
 
 
@@ -87,19 +95,34 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun seqShow(){
+
+        button.isClickable = false
+        button2.isClickable = false
+        button3.isClickable = false
+        button4.isClickable = false
+
     tv_rounds.text = rounds.toString()
             CoroutineScope(Dispatchers.IO).launch    {
                 for (i in 0..rounds-1){   // it only shows the sequence to the set number of round that is currently being played
                     delay(TimeUnit.SECONDS.toMillis(1))
                 withContext(Dispatchers.Main) {
-                  // this is called after 3 secs
+                  // this is called after 1 sec
                     btnShow(seq[i])
+                    if(i == rounds-1){
+                        CoroutineScope(Dispatchers.IO).launch    {
+                            delay(TimeUnit.MILLISECONDS.toMillis(500))
+                            withContext(Dispatchers.Main) {
+                                // this is called after 3 secs
+                                button.isClickable = true
+                                button2.isClickable = true
+                                button3.isClickable = true
+                                button4.isClickable = true
+                            }
+                        }
+                    }
                 }
             }
-        //    this is called immediately
-
         }
-
     }
 
 fun btnShow(btn: Int){
